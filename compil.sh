f0c4af3403fb4403f87225547951959cfb78e6fb
thierry <thierrytouin@gmail.com>
2020-04-30 23:51:28 +0200

    passage en SpringBoot 2.2.6 + script + eureka

---
+++ compil.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd microservice-commandes
+mvn clean package -Dmaven.test.skip=true
+cd ..
+cd microservice-paiement
+mvn clean package -Dmaven.test.skip=true
+cd ..
+cd microservice-produits
+mvn clean package -Dmaven.test.skip=true
+cd ..
+cd clientui
+mvn clean package -Dmaven.test.skip=true
+cd ..
+cd config-server
+mvn clean package -Dmaven.test.skip=true
+cd ..
+cd eureka-server
+mvn clean package -Dmaven.test.skip=true
+cd ..
+
+
