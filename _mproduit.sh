df225d86170b4bd8110632da79faea4af8aa11e6
thierry <thierrytouin@gmail.com>
2020-05-02 00:52:10 +0200

    merge ok

--- _mproduit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -jar microservice-produits/target/mproduits-0.0.1-SNAPSHOT.jar
-
-
---
+++ _mproduit_9001.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -Dserver.port=9001 -jar microservice-produits/target/mproduits-0.0.1-SNAPSHOT.jar
+
+
